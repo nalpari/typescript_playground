c0a94bc8ec729a15ec25569f7085bb6028feb4f6
유상욱/차장/(주)이마트/쓰리탑 <FAE01418@SSG.GLOBAL>
2020-04-28 10:43:32 +0900

    스터디 준비

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import { hello } from './hello';
+import $ from "jquery";
+import { hello } from "./hello";
 
 const main = (): void => {
-    console.log(hello('test222'));
+  console.log(hello("test222"));
+};
+
+const mainObj = {
+  init: function (): void {
+    this.bind();
+  },
+  bind: function (): void {
+    $("#btnCheck").off().on("click", this.methods.fnChk);
+    // document.getElementById('btnCheck')?.addEventListener('click', this.methods.fnChk, false);
+  },
+  methods: {
+    fnChk(): void {
+      console.log("click");
+    },
+  },
 };
 
 main();
+mainObj.init();
